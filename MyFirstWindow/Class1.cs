b72256763c03195aa235382829dc477b2fc88b9f
Victor Predko <Predko.Victor@gmail.com>
2017-04-18 22:53:07 +0300

    сохр/загр бинарные. недоделано

--- MyFirstWindow/Class1.cs
+++ MyFirstWindow/Class1.cs
@@ -9,26 +9,18 @@ using KSP.IO;
 
 namespace MyFirstWindow
 {
-    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
     public class MyFirstWindow : MonoBehaviour
     {
 
         readonly int _windowId = 0;
         Rect _windowRect = new Rect(100, 100, 400, 200);
-        Vector2 _scrollPosition;
-        float _sliderValue = 1;
-        string _textValue = "demo";
-        bool _toggleValue = true;
-        bool _doWindow0 = true;
 
-        readonly int _popupwindowId = 1;
-        Rect _popupRect = new Rect(500, 500, 200, 100);
-        bool _popupflag = false; // no visible
+        VesselInfo vi = new VesselInfo();
 
-        private void Awake()
-        {
-            load_cfg();
-        }
+        //private void Awake()
+        //{
+        //}
 
         private void OnDestroy()
         {
@@ -39,142 +31,159 @@ namespace MyFirstWindow
         // Загрузка сохранённых данных окна
         void load_cfg()
         {
-            PluginConfiguration config = PluginConfiguration.CreateForType<MyFirstWindow>();
-
-            //if (config != null)
-            //    return;
-
-            config.load();
-
-            _windowRect = config.GetValue<Rect>("_windowRect", new Rect(100, 100, 400, 200));
-
-            _scrollPosition = config.GetValue<Vector2>("_scrollPosition", new Vector2(0, 0));
-
-            _sliderValue = (float)config.GetValue<double>("_sliderValue", 1);
-
-            _textValue = config.GetValue<string>("_textValue", " ");
-
-            _toggleValue  = config.GetValue<Boolean>("_toggleValue", true);
-
-            _doWindow0 = config.GetValue<Boolean>("_doWindow0", true);
-
-            _popupRect = config.GetValue<Rect>("_popupRect", new Rect(500, 500, 200, 100));
-
-            _popupflag = config.GetValue<Boolean>("_popupflag", false);
+            //_windowRect = config.GetValue<Rect>("_windowRect", new Rect(100, 100, 400, 200));
         }
 
         // запись данных окна в конфиг файл
         void save_cfg()
         {
-            PluginConfiguration config = PluginConfiguration.CreateForType<MyFirstWindow>();
-
-            config.SetValue("_windowRect", _windowRect);
-
-            config.SetValue("_scrollPosition", _scrollPosition);
-
-            config.SetValue("_sliderValue", (double)_sliderValue);
-
-            config.SetValue("_textValue", _textValue);
-
-            config.SetValue("_toggleValue", _toggleValue);
-
-            config.SetValue("_doWindow0", _doWindow0);
-
-            config.SetValue("_popupRect", _popupRect);
-
-            config.SetValue("_popupflag", _popupflag);
-
-            config.save();
+            //config.SetValue("_windowRect", _windowRect);
 
         }
 
         void OnGUI()
         {
-            // переключатель видимости окна Basic Window
-            _doWindow0 = GUI.Toggle(new Rect(_windowRect.x, _windowRect.y - 30, 100, 20), _doWindow0, "Window 0");
-
-            if (_doWindow0)
-            {
-                // отображение окна
-                _windowRect = GUI.Window(_windowId, _windowRect, DoWindow0, "Basic Window");
-                if (_windowRect.y < 30)
-                    _windowRect.y = 30;
-            }
-
-            if (_popupflag)
-            {
-                // Отображение окна
-                _popupRect = GUILayout.Window(_popupwindowId, _popupRect, DoPopupWindow, "Info");
-            }
-
+            // отображение окна
+            _windowRect = GUILayout.Window(_windowId, _windowRect, DoWindow0, "Vessel info");
+            if (_windowRect.y < 30)
+                _windowRect.y = 30;
         }
 
         // Окно(главное) "Basic Window"
         void DoWindow0(int windowID)
         {
-            _scrollPosition = GUILayout.BeginScrollView(_scrollPosition);
-
             GUILayout.BeginVertical(GUILayout.ExpandHeight(true));
 
             GUILayout.BeginHorizontal();
 
             GUILayout.FlexibleSpace();
 
-            if (GUILayout.Button("Press me"))
+            if (GUILayout.Button("Save"))
             {
-                _popupflag = true;
+                save_cfg();
+                ScreenMessages.PostScreenMessage("Info saved", .5f, ScreenMessageStyle.LOWER_CENTER);
+            }
+
+
+            if (GUILayout.Button("Load"))
+            {
+                load_cfg();
+                ScreenMessages.PostScreenMessage("Info loaded", .5f, ScreenMessageStyle.LOWER_CENTER);
             }
 
             GUILayout.FlexibleSpace();
 
             GUILayout.EndHorizontal();
 
-            _sliderValue = GUILayout.HorizontalSlider(_sliderValue, 0, 10);
+            GUILayout.Space(10);
 
-            GUILayout.Label("Simple text");
+            vi.SetInfo(FlightGlobals.ActiveVessel);
 
-            if (GUILayout.RepeatButton("Simple repeat button"))
+            int la = (int)GUI.skin.label.alignment;
+
+            GUI.skin.label.alignment = TextAnchor.MiddleRight;
+
+            using (var hs = new GUILayout.HorizontalScope("scale"))
             {
-                ScreenMessages.PostScreenMessage("Repeat button pressed!", .5f, ScreenMessageStyle.LOWER_CENTER);
+
+                for (int i = 1; i != 10; i++)
+                    GUILayout.Label(String.Format("{0}0", i), GUILayout.Width(10));
             }
 
-            GUILayout.TextArea("Demo text");
 
-            _textValue = GUILayout.TextField(_textValue);
+            using (var hs = new GUILayout.HorizontalScope("Name vessel"))
+            {
+                GUILayout.Label("    Name vessel:");
+                GUILayout.TextArea(vi.name);
+            }
 
-            _toggleValue = GUILayout.Toggle(_toggleValue, "Simple toggle");
+            using (var hs = new GUILayout.HorizontalScope("Altitude"))
+            {
 
-            GUILayout.VerticalSlider(_sliderValue, 0, 10);
+                GUILayout.Label("       Altitude:");
+                GUILayout.TextArea(string.Format("{0,-10:f2}",vi.Altitude));
+            }
+
+            using (var hs = new GUILayout.HorizontalScope("Gee force"))
+            {
+                GUILayout.Label("      Gee force:");
+                GUILayout.TextArea(string.Format("{0,-10:f2}", vi.gforce));
+            }
+
+            using (var hs = new GUILayout.HorizontalScope("Surface speed"))
+            {
+                GUILayout.Label("  Surface speed:");
+                GUILayout.TextArea(string.Format("{0,-10:f2}", vi.surf_speed));
+            }
+
+            using (var hs = new GUILayout.HorizontalScope("Vertical speed"))
+            {
+                GUILayout.Label(" Vertical speed:");
+                GUILayout.TextArea(string.Format("{0,-10:f2}", vi.vert_speed));
+            }
+
+            GUI.skin.label.alignment = (TextAnchor)la;
 
             GUILayout.EndVertical();
 
-            GUILayout.EndScrollView();
-
             GUI.DragWindow(new Rect(0, 0, 10000, 20));
         }
-
-        // вспомогательное окно "Info" 
-        void DoPopupWindow(int _windowid)
-        {
-            GUILayout.Label("Save and load config");
-            if (GUILayout.Button("Save"))
-            {
-                _popupflag = false;
-                save_cfg(); // записать данные окон
-            }
-
-            if (GUILayout.Button("Load"))
-            {
-                _popupflag = false;
-                load_cfg(); // загрузить данные окон
-            }
-
-            if (GUILayout.Button("Cancel"))
-            {
-                _popupflag = false; // просто закрыть вспомогательное окно
-            }
-            GUI.DragWindow();
-        }
     }
+
+    // вспомогательное окно 
+    void DoPopupWindow(int _windowid)
+    {
+        GUILayout.Label(string.Format("width = {0,10:d}", GUILayout.);
+        if (GUILayout.Button("Save"))
+        {
+            _popupflag = false;
+            save_cfg(); // записать данные окон
+        }
+
+        if (GUILayout.Button("Load"))
+        {
+            _popupflag = false;
+            load_cfg(); // загрузить данные окон
+        }
+
+        if (GUILayout.Button("Cancel"))
+        {
+            _popupflag = false; // просто закрыть вспомогательное окно
+        }
+        GUI.DragWindow();
+    }
+}
+
+internal class VesselInfo
+    {
+        public double Altitude { get; set; }
+        public double gforce { get; set; }
+        public double surf_speed { get; set; }
+        public double vert_speed { get; set; }
+        public string name { get; set; }
+
+        public VesselInfo()
+        {
+            Altitude = gforce = surf_speed = vert_speed = 0;
+            name = " ";
+        }
+
+        public void SetInfo(Vessel v)
+        {
+            Altitude = v.altitude;
+            gforce = v.geeForce;
+            surf_speed = v.srfSpeed;
+            vert_speed = v.verticalSpeed;
+            name = v.name;
+        }
+
+        public string ToString()
+        {
+            string s = "";
+            s = name + Altitude + gforce + surf_speed + vert_speed;
+            return s;
+        }
+    } 
+
 }
 
